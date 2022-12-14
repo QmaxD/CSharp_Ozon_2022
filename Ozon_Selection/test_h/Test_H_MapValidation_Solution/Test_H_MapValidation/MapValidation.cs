d9754a220025b8a5f7e69f6206ade931b31c2421
Qmax <QmaxD@users.noreply.github.com>
2022-12-14 08:47:33 +0400

    First commit
    - create structure,
    - create README.md,
    - add Tests for all test folder,
    - add complete test_H in Ozon_Selection

---
+++ Ozon_Selection/test_h/Test_H_MapValidation_Solution/Test_H_MapValidation/MapValidation.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_H_MapValidation
+{
+    public static class MapValidation
+    {
+        public static void StartMapValidation(string inputFile, string outputFile) {
+
+            string? inputLine;
+            try
+            {
+                StreamReader inputSR = new(inputFile);
+                StreamWriter outSR = new(outputFile);
+                // ================================================== time ===============
+                Stopwatch stopwatch = new();
+                stopwatch.Start();
+
+                inputLine = inputSR.ReadLine();
+                int numberOfTests = int.Parse(inputLine);
+                //Console.ForegroundColor = ConsoleColor.Yellow;
+                //Console.WriteLine($"{numberOfTests} test {str1}");
+
+                do
+                {
+                    inputLine = inputSR.ReadLine();
+                    int[] size = inputLine.Split(' ').Select(it => int.Parse(it)).ToArray();
+                    int row = size[0];
+                    int col = size[1];
+                    //Console.ForegroundColor = ConsoleColor.Yellow;
+                    //Console.Write($"\n{row} {col} ");
+                    //Console.WriteLine($"map[{row}][{(col + 1) / 2 + (row - 1) / 2}]");
+                    //Console.ForegroundColor = ConsoleColor.White;
+
+                    char[][] map = new char[row][];
+                    for (int i = 0; i < row; i++)
+                    {
+                        map[i] = new char[(col + 1) / 2 + (row - 1) / 2];
+                    }
+
+                    int numberRow = 0;
+                    int numberCol = (row - 1) / 2;
+                    int oddRow = 0;
+                    do
+                    {
+                        inputLine = inputSR.ReadLine();
+                        inputLine = inputLine.Trim('.');
+                        char[] rowColor = inputLine.Split('.').Select(it => char.Parse(it)).ToArray();
+                        int indexRowColor = 0;
+
+                        for (int width = 0; width < map[numberRow].Length; width++)
+                        {
+                            if (width < numberCol || indexRowColor >= rowColor.Length)
+                                map[numberRow][width] = '.';
+                            else
+                            {
+                                map[numberRow][width] = rowColor[indexRowColor];
+                                indexRowColor++;
+                            }
+                        }
+
+                        oddRow++;
+                        if (oddRow % 2 == 0)
+                            numberCol--;
+                        numberRow++;
+
+                        row--;
+                    } while (row > 0);
+
+                    //PrintMap1();// печать полученного массива map[][]
+                    //PrintMap2();// печать полученного массива map[][]
+
+                    // поиск границ цвета
+                    bool allTrue = true;
+                    List<char> colors = new();
+                    char color = '.';
+
+                    int h = 0, w = 0;
+                    while (h < map.Length && allTrue)
+                    {
+                        w = 0;
+                        while (w < map[h].Length && allTrue)
+                        {
+                            if (map[h][w] != '.' && map[h][w] != 'x')
+                            {
+                                color = map[h][w];
+                                map[h][w] = 'x';
+
+                                if (colors.Contains(color))
+                                {
+                                    //Console.ForegroundColor = ConsoleColor.Red;
+                                    //Console.WriteLine("NO\n");
+                                    allTrue = false;
+                                }
+                                else
+                                {
+                                    //Console.ForegroundColor = ConsoleColor.Yellow;
+                                    //Console.WriteLine("-- search color " + color + " --");
+                                    SearchColorRegion(map, h, w, color);
+                                    //Console.ForegroundColor = ConsoleColor.Green;
+                                    //Console.WriteLine("YES\n");
+                                    //PrintMap1();
+                                    //PrintMap2();
+                                }
+
+                                colors.Add(color);// запишем цвет в массив
+                            }
+
+                            w++;
+                        }
+
+                        h++;
+                    }
+
+                    if (allTrue)
+                    {
+                        //Console.ForegroundColor = ConsoleColor.Green;
+                        //Console.WriteLine("ПОЛНОЕ СОВПАДЕНИЕ");
+                        //Console.WriteLine("----------------------------------------------------");
+                        outSR.WriteLine("YES");
+                    }
+                    else
+                    {
+                        //Console.ForegroundColor = ConsoleColor.Red;
+                        //Console.WriteLine("ERROR");
+                        //Console.WriteLine("----------------------------------------------------");
+                        outSR.WriteLine("NO");
+                    }
+
+                    numberOfTests--;
+                }
+                while (numberOfTests > 0);
+
+                //Console.ForegroundColor = ConsoleColor.White;
+                // ================================================== time ===============           
+                stopwatch.Stop();
+                TimeSpan ts = stopwatch.Elapsed;
+                Console.WriteLine("\ntime: " + ts);
+
+                inputSR.Close();
+                outSR.Close();
+            }
+            catch (Exception e) { Console.WriteLine("\nException: " + e.Message); }
+            finally { Console.WriteLine("Executing finally block."); }
+
+            void PrintMap_version_1(char[][] map)
+            {
+                for (int h = 0; h < map.Length; h++)
+                {
+                    //if (h % 2 != 0)
+                    //{
+                    //    Console.Write(" ");
+                    //}
+                    for (int w = 0; w < map[h].Length; w++)
+                    {
+                        //if (map[h][w] != '.')
+                        //{
+                        if (map[h][w] == 'x')
+                            Console.ForegroundColor = ConsoleColor.Magenta;
+                        else
+                            Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write(map[h][w] + " ");
+                        //}
+                    }
+                    Console.WriteLine("");
+                }
+                Console.WriteLine("");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
+            void PrintMap_version_2(char[][] map)
+            {
+                for (int h = 0; h < map.Length; h++)
+                {
+                    if (h % 2 != 0)
+                    {
+                        Console.Write(" ");
+                    }
+                    for (int w = 0; w < map[h].Length; w++)
+                    {
+                        if (map[h][w] != '.')
+                        {
+                            if (map[h][w] == 'x')
+                                Console.ForegroundColor = ConsoleColor.Magenta;
+                            else
+                                Console.ForegroundColor = ConsoleColor.White;
+                            Console.Write(map[h][w] + " ");
+                        }
+                    }
+                    Console.WriteLine("");
+                }
+                Console.WriteLine("");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
+            void SearchColorRegion(char[][] map, int row, int col, char color)
+            {
+                char verified = 'x';
+
+                // ---  проверка в ряду справа  ---
+                if (col + 1 < map[row].Length)
+                {
+                    if (map[row][col + 1] == color)
+                    {
+                        map[row][col + 1] = verified;
+                        //PrintMap2();
+                        SearchColorRegion(map, row, col + 1, color);
+                    }
+                }
+
+                // ---  проверка внизу справа  ---
+                if (row + 1 < map.Length)
+                {
+                    if (map[row + 1][col] == color)
+                    {
+                        map[row + 1][col] = verified;
+                        //PrintMap2();
+                        SearchColorRegion(map, row + 1, col, color);
+                    }
+                }
+
+                // ---  проверка внизу слева  ---
+                if (row + 1 < map.Length && col - 1 >= 0)
+                {
+                    if (map[row + 1][col - 1] == color)
+                    {
+                        map[row + 1][col - 1] = verified;
+                        //PrintMap2();
+                        SearchColorRegion(map, row + 1, col - 1, color);
+                    }
+                }
+
+                // ---  проверка в ряду слева  ---
+                if (col - 1 >= 0)
+                {
+                    if (map[row][col - 1] == color)
+                    {
+                        map[row][col - 1] = verified;
+                        //PrintMap2();
+                        SearchColorRegion(map, row, col - 1, color);
+                    }
+                }
+
+                // ---  проверка вверху слева  ---
+                if (row - 1 >= 0)
+                {
+                    if (map[row - 1][col] == color)
+                    {
+                        map[row - 1][col] = verified;
+                        //PrintMap2();
+                        SearchColorRegion(map, row - 1, col, color);
+                    }
+                }
+
+                // ---  проверка вверху справа  ---
+                if (row - 1 >= 0 && col + 1 < map[row - 1].Length)
+                {
+                    if (map[row - 1][col + 1] == color)
+                    {
+                        map[row - 1][col + 1] = verified;
+                        //PrintMap2();
+                        SearchColorRegion(map, row - 1, col + 1, color);
+                    }
+                }
+
+            }
+
+        }
+    }
+}
---
+++ Ozon_Selection/test_h/Test_H_MapValidation_Solution/Test_H_MapValidation/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;// LeadTime
+using System.Linq;
+using System.Text.RegularExpressions;// Regex, Validation
+
+namespace Test_H_MapValidation
+{
+    public class Program
+    {
+        public static void Main(string[] args)// всё проходит менее чем 2 сек.
+        {
+            string numberTest = "25";
+            string letterTest = "h";
+            string inputFile = "E:\\Microsoft_Visual_Studio\\Project\\Ozon_selection\\pink_wolf_c43a_Sel_" + letterTest + "\\tests\\" + numberTest;
+            string originFile = "E:\\Microsoft_Visual_Studio\\Project\\Ozon_selection\\pink_wolf_c43a_Sel_" + letterTest + "\\tests\\" + numberTest + ".a";
+            string outputFile = "E:\\Microsoft_Visual_Studio\\Project\\Ozon_selection\\pink_wolf_c43a_Sel_" + letterTest + "\\tests\\" + numberTest + "out.a";
+
+            MapValidation.StartMapValidation(inputFile, outputFile);
+
+            bool resultCompareFiles = FileCompare.StartFilesCompare(originFile, outputFile);
+            if (resultCompareFiles)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\nCOMPARE FILES: " + (resultCompareFiles.ToString()).ToUpper() + " !");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nCOMPARE FILES: " + (resultCompareFiles.ToString()).ToUpper() + " !");
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+
+        }
+
+    }
+}
